74a0d7a1946365b9633b8c646f1dfd56c2e03298
Maurice <maurice.ronet.dominguez@gmail.com>
2019-12-13 09:23:15 +0100

    refactor: solvet hlint hints

--- src/Main.hs
+++ src/Main.hs
@@ -10,19 +10,19 @@ data User = User Username Password deriving (Eq, Show)
 
 -- Predicates
 hasMaxLength :: Int -> String -> Bool 
-hasMaxLength n s = (length s) <= n
+hasMaxLength n s = length s <= n
 
 isAlphaNumeric :: String -> Bool
-isAlphaNumeric = (all isAlphaNum)
+isAlphaNumeric = all isAlphaNum
 
 hasSpace :: String -> Bool
 hasSpace = any isSpace
 
 -- Util Either
 toEitherValidation :: (a -> Bool) -> Error -> a -> Either Error a
-toEitherValidation predicate validationMessage x = case (predicate x) of
-                                                    True -> Right x
-                                                    False -> Left validationMessage
+toEitherValidation predicate validationMessage x | predicate x = Right x
+                                                 | otherwise = Left validationMessage
+
 --Validations
 validateHasSpace :: String -> Either Error String
 validateHasSpace = toEitherValidation (not . hasSpace) (Error "It does not have to contain spaces")
@@ -46,8 +46,8 @@ validatePassword password = validateHasSpace password
 
 validateCredentials :: String -> String -> Either Error User
 validateCredentials username password = User <$> username' <*> password' where
-                                        username' = Username <$> (validateUsername username)
-                                        password' = Password <$> (validatePassword password)
+                                        username' = Username <$> validateUsername username
+                                        password' = Password <$> validatePassword password
 
 -- Main
 main :: IO ()
